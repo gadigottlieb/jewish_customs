ab0e6ed61e31997fe87ae69e8d675cfae967869d
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-09-10 11:14:47 -0400

    Changed title, started working on tests showing wrong answers

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -14,4 +14,19 @@ class TestResult < ActiveRecord::Base
     end
     score = (correct_answers.to_f / (correct_answers+wrong_answers).to_f) * 100
   end
+
+  def show_correct_answer(test, answers)
+    correct_answers = []
+    wrong_answers = []
+    @test = Test.find(test)
+    tests_answers = @test.questions.map do |a|
+      a.correct_answer
+    end
+    tests_answers.each_with_index do |el, i|
+      el == answers[i] ? correct_answers << el : wrong_answers << el
+    end
+    wrong_answers.each do |a|
+      a
+    end
+  end
 end
